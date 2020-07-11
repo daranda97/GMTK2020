dcd085059e3fce4112347329b5a9691b042732d7
szyzygy <alden.towler@student.nmt.edu>
2020-07-11 10:37:25 -0600

    added player overlord prefab and some code to test walking correction

---
+++ GMTKGame/Assets/Scripts/Leg_orient_script.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leg_orient_script : MonoBehaviour
+{
+
+    public List<GameObject> walking_axes;
+    public GameObject marionet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        marionet.transform.LookAt(walking_axes[0].transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.W) && Input.GetKeyDown(KeyCode.A))
+        {
+
+            marionet.transform.LookAt(walking_axes[1].transform.position);
+
+        }
+        if (Input.GetKeyDown(KeyCode.W) && Input.GetKeyDown(KeyCode.D))
+        {
+
+            marionet.transform.LookAt(walking_axes[3].transform.position);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+
+            marionet.transform.LookAt(walking_axes[2].transform.position);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+            marionet.transform.LookAt(walking_axes[0].transform.position);
+
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+
+            marionet.transform.LookAt(walking_axes[4].transform.position);
+
+        }
+        
+
+    }
+
+
+}
