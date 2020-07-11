229796066014c64780382890114e7da0b85f3901
bughunterstudios <bughunterstudios@gmail.com>
2020-07-11 16:55:35 -0600

    Enemies can now die.

--- GMTKGame/Assets/Scripts/KillThings.cs
+++ GMTKGame/Assets/Scripts/KillThings.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class KillThings : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool killing;
+    public GameObject shattered_enemy;
+    public GameObject shattered_player;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (killing)
+        {
+            //Will need to make the camera separate
+            /*if (other.tag == "Player")
+            {
+                GameObject newshatter = Instantiate(shattered_player);
+                newshatter.transform.position = other.transform.position;
+                newshatter.transform.rotation = other.transform.rotation;
+                Destroy(other.gameObject);
+            }*/
+            if (other.tag == "Enemy")
+            {
+                GameObject newshatter = Instantiate(shattered_enemy);
+                newshatter.transform.position = other.transform.position;
+                newshatter.transform.rotation = other.transform.rotation;
+                Destroy(other.gameObject);
+            }
+        }
     }
 }
