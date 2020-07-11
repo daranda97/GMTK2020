89f11afe4122db876c7b14b8eaf3615d8f1303ab
szyzygy <alden.towler@student.nmt.edu>
2020-07-10 21:44:22 -0600

    adding some character controller scripts

---
+++ GMTKGame/Assets/Scripts/Character control.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charactercontrol : MonoBehaviour
+{
+
+
+
+    public float move_speed;
+    public bool move_lock;
+    CharacterController cr;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void Move()
+    {
+
+        float horizontal_in = Input.GetAxis("Horizontal") * move_speed;
+        float vertical_in = Input.GetAxis("Vertical") * move_speed;
+
+        Vector3 move_forward = transform.forward * vertical_in;
+        Vector3 move_sideways = transform.right * horizontal_in;
+
+        cr.SimpleMove(move_forward + move_sideways);
+
+
+    }
+
+
+
+
+}
