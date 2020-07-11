a388de0622bce422158bfb6d5c0bfdb863f9944b
szyzygy <alden.towler@student.nmt.edu>
2020-07-11 11:23:29 -0600

    added some enemy ai

---
+++ GMTKGame/Assets/Scripts/enemy_nav_enemy_navigation_control.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemy_nav_enemy_navigation_control : MonoBehaviour
+{
+ public Transform target;
+
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(target.transform.position,agent.transform.position) < 50)
+            agent.SetDestination(target.position);
+    }
+
+
+    void shoot() {
+
+
+        // add shooty code here plz
+
+    }
+}
