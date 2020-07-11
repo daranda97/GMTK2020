9c020e36ba6308487ba2baf75c94228b5d87518c
daranda97 <48872258+daranda97@users.noreply.github.com>
2020-07-11 16:23:18 -0600

    menu script

---
+++ GMTKGame/Assets/Scripts/StartMenuControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuControl : MonoBehaviour
+{
+
+
+
+    void ChangeScene()
+    {
+        SceneManager.LoadScene("SceneName", LoadSceneMode.single);
+    }
+
+}
