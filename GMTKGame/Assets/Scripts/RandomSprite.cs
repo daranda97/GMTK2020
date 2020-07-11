1c6c7476b951e13ef07bff0df35457a8e8277b3a
bughunterstudios <bughunterstudios@gmail.com>
2020-07-11 12:48:51 -0600

    Imported flash effect.

---
+++ GMTKGame/Assets/Scripts/RandomSprite.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class RandomSprite : MonoBehaviour
+{
+    public List<Sprite> sprites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Count)];
+    }
+}
