b42efb1f65f0424ac9211a15d94d883deef42cb1
bughunterstudios <bughunterstudios@gmail.com>
2020-07-11 09:45:33 -0600

    Updated some scripts. And updated quadcopter.

--- GMTKGame/Assets/Scripts/Rot_char_caharacter_rotation_control.cs
+++ GMTKGame/Assets/Scripts/Rot_char_caharacter_rotation_control.cs
@@ -4,17 +4,10 @@ using UnityEngine;
 
 public class Rot_char_caharacter_rotation_control : MonoBehaviour
 {
-    Vector2 mouse_look;
-    Vector2 Smoothing;
-    public float rotation_sensitivity = 2f;
-    public float smoothing_factor = 2f;
     public bool halt_mouse_control;
-    public GameObject main_camera;
 
     RaycastHit hit;
     Ray ray;
-    //*/
-
 
     public GameObject character_box;
     
@@ -35,23 +28,14 @@ public class Rot_char_caharacter_rotation_control : MonoBehaviour
         if (!halt_mouse_control)
         {
 
-            ray = main_camera.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
             if (Physics.Raycast(ray, out hit, 1000))
             {
-
-
                 Vector3 direction = new Vector3(hit.point.x, transform.position.y, hit.point.z) - transform.position;
                 Quaternion toRotation = Quaternion.LookRotation(direction);
                 character_box.transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, speed * Time.time);
-
-
-
             }
-
-
-
-
         }
     }
 }
---
+++ GMTKGame/Assets/Scripts/RotateTowardsMouse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTowardsMouse : MonoBehaviour
+{
+    private RaycastHit hit;
+    private Ray ray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = main_camera.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 1000))
+        {
+            Vector3 direction = new Vector3(hit.point.x, transform.position.y, hit.point.z) - transform.position;
+            Quaternion toRotation = Quaternion.LookRotation(direction);
+            character_box.transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, speed * Time.time);
+        }
+    }
+}
