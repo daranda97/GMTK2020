8f4aa79cc6c5e12224c526b2048a5266e8a9356c
bughunterstudios <bughunterstudios@gmail.com>
2020-07-11 22:02:47 -0600

    Added time control.

---
+++ GMTKGame/Assets/Scripts/TimeControl.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeControl : MonoBehaviour
+{
+    public float normaltime;
+    public float slowtime;
+    public float changespeed;
+
+    private bool decreasing;
+    private float fixedDeltaTime;
+    private float targetslow;
+    private float currentchangespeed;
+
+
+    private bool paused;
+    private float ts;
+
+    void Awake()
+    {
+        fixedDeltaTime = Time.fixedDeltaTime;
+    }
+
+    public void Pause()
+    {
+        if (paused)
+        {
+            Time.timeScale = ts;
+            Time.fixedDeltaTime = ts * fixedDeltaTime;
+            paused = false;
+        }
+        else
+        {
+            paused = true;
+            ts = Time.timeScale;
+            Time.timeScale = 0;
+            Time.fixedDeltaTime = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!paused)
+        {
+            if (decreasing)
+            {
+                if (Time.timeScale > targetslow)
+                {
+                    Time.timeScale -= currentchangespeed * Time.deltaTime;
+                    Time.fixedDeltaTime = fixedDeltaTime * Time.timeScale;
+                }
+                else
+                    decreasing = false;
+            }
+            else
+            {
+                if (Time.timeScale < normaltime)
+                {
+                    Time.timeScale += currentchangespeed * Time.deltaTime;
+                    Time.fixedDeltaTime = fixedDeltaTime * Time.timeScale;
+                }
+                else
+                {
+                    Time.timeScale = normaltime;
+                    Time.fixedDeltaTime = fixedDeltaTime * Time.timeScale;
+                }
+            }
+        }
+    }
+
+    private void Trigger()
+    {
+        decreasing = true;
+        targetslow = slowtime;
+        currentchangespeed = changespeed;
+    }
+}
