7ee23c42697865c358c3455426759c3863e6379d
Damon Estrada <damon.estrada@student.nmt.edu>
2020-07-12 10:00:34 -0600

    Enemy 2 Control Script
    A copy of the enemy1 script except with bullets changes.

---
+++ GMTKGame/Assets/Enemy_nav_enemy2_navigation_control.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy_nav_enemy2_navigation_control : MonoBehaviour
+{
+    public Transform target;
+
+    private NavMeshAgent agent;
+    public float rot_speed;
+
+    private float shot_stopwatch;
+    public GameObject bullet_prefab;
+    public GameObject end_of_barrel;
+    public float refire_time;
+    public float firevelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        shot_stopwatch = refire_time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            if (Vector3.Distance(target.position, transform.position) < 50)
+            {
+                agent.SetDestination(target.position);
+
+
+                shot_stopwatch += Time.deltaTime;
+
+                if (shot_stopwatch >= refire_time)
+                {
+                    shoot();
+                }
+
+                Vector3 direction = target.position - transform.position;
+                Quaternion toRotation = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, rot_speed * Time.deltaTime);
+
+            }
+            else
+            {
+
+
+                agent.SetDestination(transform.position);
+                // add patroling code here if applicable
+            }
+
+
+        }
+
+
+    }
+
+
+
+    void shoot()
+    {
+
+
+
+        shot_stopwatch = 0;
+        GameObject newbullet = Instantiate(bullet_prefab);
+        newbullet.transform.position = end_of_barrel.transform.position;
+        newbullet.transform.rotation = end_of_barrel.transform.rotation;
+
+        newbullet.GetComponent<Rigidbody>().AddForce(newbullet.transform.forward * firevelocity);
+
+
+
+
+
+    }
+}
