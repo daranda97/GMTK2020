0fe9b840346ae74c9d2c369ad36b3c8c79b66c1d
bughunterstudios <bughunterstudios@gmail.com>
2020-07-11 18:50:07 -0600

    Camera follows player. And the player dies.

--- GMTKGame/Assets/Scripts/CameraFollow.cs
+++ GMTKGame/Assets/Scripts/CameraFollow.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform player;
+    public float speed;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.position.x, transform.position.y, player.position.z), speed * Time.deltaTime);
     }
 }
