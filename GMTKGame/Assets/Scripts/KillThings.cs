d12761d193ffce03c03c41e8b222698c5d3a3a8e
Isaiah-Liljestrand <46874078+Isaiah-Liljestrand@users.noreply.github.com>
2020-07-11 21:18:30 -0600

    update on rocket controlling

--- GMTKGame/Assets/Scripts/KillThings.cs
+++ GMTKGame/Assets/Scripts/KillThings.cs
@@ -7,7 +7,6 @@ public class KillThings : MonoBehaviour
     public GameObject shattered_enemy;
     public GameObject shattered_player;
     public bool youngBullet = false;
-    private float timepassed;
 
 
     private void OnTriggerEnter(Collider other)
