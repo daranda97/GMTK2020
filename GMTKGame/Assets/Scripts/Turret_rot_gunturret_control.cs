6d5439ccd9e3897e372d3ce576ca340f8cf0648b
szyzygy <alden.towler@student.nmt.edu>
2020-07-11 19:49:58 -0600

    added a return function to the turret

--- GMTKGame/Assets/Scripts/Turret_rot_gunturret_control.cs
+++ GMTKGame/Assets/Scripts/Turret_rot_gunturret_control.cs
@@ -6,6 +6,8 @@ public class Turret_rot_gunturret_control : MonoBehaviour
 {
     public float input_z;
     public float speed;
+    public int counter;
+    public bool fire_limiter;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,25 @@ public class Turret_rot_gunturret_control : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.localRotation = Quaternion.RotateTowards(transform.localRotation, Quaternion.Euler(0, 0, input_z), speed * Time.deltaTime);
+
+
+       
+            
+            
+
+       
+
+        
     }
+
+
+    void rotate() {
+
+
+        transform.localRotation = Quaternion.RotateTowards(transform.localRotation, Quaternion.Euler(0, 0, input_z), speed * Time.deltaTime);
+
+
+
+    }
+
 }
