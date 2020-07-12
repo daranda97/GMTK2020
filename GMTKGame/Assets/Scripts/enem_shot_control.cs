1cc2ccca5d1570b3202bc843568510de43d3200a
szyzygy <alden.towler@student.nmt.edu>
2020-07-11 18:06:31 -0600

    Update enem_shot_control.cs

--- GMTKGame/Assets/Scripts/enem_shot_control.cs
+++ GMTKGame/Assets/Scripts/enem_shot_control.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class enem_shot_control : MonoBehaviour
 {
+
+    private float shot_stopwatch;
+    public GameObject bullet_prefab;
+    public GameObject end_of_barrel;
+    public float refire_time;
+    public float firevelocity;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,18 @@ public class enem_shot_control : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        shot_stopwatch += Time.deltaTime;
+
+        if (shot_stopwatch >= refire_time)
+        {
+            shot_stopwatch = 0;
+            GameObject newbullet = Instantiate(bullet_prefab);
+            newbullet.transform.position = end_of_barrel.transform.position;
+            newbullet.transform.rotation = end_of_barrel.transform.rotation;
+
+            newbullet.GetComponent<Rigidbody>().AddForce(newbullet.transform.forward * firevelocity);
+        }
+
     }
 }
