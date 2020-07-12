b8a986bdf8cb0aa7b08bc71d502555f7b58cc2fa
Isaiah-Liljestrand <46874078+Isaiah-Liljestrand@users.noreply.github.com>
2020-07-12 01:13:20 -0600

    rocket splitting implemented

--- GMTKGame/Assets/Scripts/RocketController.cs
+++ GMTKGame/Assets/Scripts/RocketController.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class RocketController : MonoBehaviour
 {
     public Vector3 target;
+    public GameObject bullet_prefab;
     public GameObject enemy;
-    public float young_time;
+    public float time = 0;
+    private bool flag = true;
+    public float velocity;
 
     private void Start()
     {
@@ -16,10 +19,11 @@ public class RocketController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        young_time -= Time.deltaTime;
-        if(young_time < 0)
+        time += Time.deltaTime;
+        if(flag && time > 1.0f)
         {
             GetComponentInChildren<KillThings>().youngBullet = false;
+            flag = false;
         }
         //Update targeting information
         if ((enemy != null) && enemy.activeInHierarchy)
@@ -28,13 +32,23 @@ public class RocketController : MonoBehaviour
         }
 
         //in case of wanting to replace with regular bullets
-        /*if (Vector3.Distance(target, rocket.transform.position) < cancelDistance)
+        if (time > 4.0f)
         {
-            //create 2 bullet that are not young in same place with slightly split trajectory (20 degree split) and same speed
-            //delete this rocket
-
+            Object.Destroy(this.gameObject);
+            GameObject newbullet1 = Instantiate(bullet_prefab);
+            GameObject newbullet2 = Instantiate(bullet_prefab);
+            newbullet1.GetComponent<BulletControl>().youngBullet = false;
+            newbullet1.GetComponent<BulletControl>().youngBullet = false;
+            newbullet1.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 2f);
+            newbullet1.transform.forward = transform.forward;
+            newbullet2.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 2f);
+            newbullet2.transform.forward = transform.forward;
+            newbullet1.transform.Rotate(transform.up, 45);
+            newbullet2.transform.Rotate(transform.up, -45);
+            newbullet1.GetComponent<Rigidbody>().AddForce(newbullet1.transform.forward * velocity);
+            newbullet2.GetComponent<Rigidbody>().AddForce(newbullet2.transform.forward * velocity);
             return;
-        }*/
+        }
 
         GetComponent<Rigidbody>().AddForce(-transform.forward * 140);
         transform.LookAt(target);
